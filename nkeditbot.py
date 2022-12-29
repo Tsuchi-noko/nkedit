af11c8b18a17f11924bf0cc992669737ab6137ad
Tsuchi-noko <91912444+Tsuchi-noko@users.noreply.github.com>
2022-12-29 15:49:06 -0300

    Core File
    Prototype 0.1

---
+++ nkeditbot.py
@@ -0,0 +1,36 @@
+from telegram.ext import Updater, filters, CallbackContext 
+from telegram.ext import JobQueue
+from telegram.ext import MessageHandler, Filters
+import telegram
+
+update_queue = JobQueue()
+bot = telegram.Bot("tokengoeshere")
+updater = Updater(bot=bot, update_queue=update_queue)
+
+def edit_hashtags(update, context):
+    bot = context.bot
+    message = update.message
+    chat_id = message.chat_id
+
+    # Prompt the user for the channel and the text to replace
+    bot.send_message(chat_id=chat_id, text="Enter the username of the channel (e.g. @Kemofure)")
+    channel = context.bot.wait_for_message(chat_id=chat_id).text
+    bot.send_message(chat_id=chat_id, text="Enter the text to replace (e.g. #jungle_crow)")
+    old_text = context.bot.wait_for_message(chat_id=chat_id).text
+    bot.send_message(chat_id=chat_id, text="Enter the new text (e.g. #Large_billed_crow)")
+    new_text = context.bot.wait_for_message(chat_id=chat_id).text
+
+    # Get the channel ID
+    channel_id = bot.get_chat(channel).id
+
+    # Iterate through the messages in the channel
+    for message in bot.iter_history(channel_id):
+        # Check if the message is an image with a caption
+        if message.photo and message.caption:
+            # Replace the old text with the new text in the caption
+            caption = message.caption.replace(old_text, new_text)
+
+            # Edit the caption of the message
+            bot.edit_message_caption(chat_id=channel_id, message_id=message.message_id, caption=caption)
+
+       
